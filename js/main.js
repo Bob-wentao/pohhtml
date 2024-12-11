ceaa3721c4a6e140d1008129af63aae0292c6e63
Bob-wentao <143519535+Bob-wentao@users.noreply.github.com>
2024-12-11 23:31:51 +0800

    finall

--- js/main.js
+++ js/main.js
@@ -24,35 +24,7 @@
 */
 const timeline = gsap.timeline({ repeat: -1 });
 const chars = document.querySelectorAll(".text10");
-gsap.registerPlugin(ScrollTrigger);
 
-/* SMOOTH SCROLL */
-const scroller = new LocomotiveScroll({
-    el: document.querySelector(".container"),
-    smooth: true
-});
-
-scroller.on("scroll", ScrollTrigger.update);
-
-ScrollTrigger.scrollerProxy(".container", {
-    scrollTop(value) {
-        return arguments.length
-            ? scroller.scrollTo(value, 0, 0)
-            : scroller.scroll.instance.scroll.y;
-    },
-    getBoundingClientRect() {
-        return {
-            left: 0,
-            top: 0,
-            width: window.innerWidth,
-            height: window.innerHeight
-        };
-    }
-});
-
-ScrollTrigger.addEventListener("refresh", () => scroller.update());
-
-ScrollTrigger.refresh();
 
 /* COLOR CHANGER */
 window.addEventListener("load", function () {
